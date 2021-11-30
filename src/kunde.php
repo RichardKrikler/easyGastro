56d831a4d1ae3df03144025cd5a5da365b4ba728
Bierkenig <92359399+Bierkenig@users.noreply.github.com>
2021-11-30 11:45:33 +0100

    kunde.php

---
+++ src/kunde.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace easyGastro;
+
+require_once 'SiteTemplate.php';
+require_once 'Pages.php';
+require_once 'db.php';
+require_once 'DB_User.php';
+
+
+session_start();
+
+$nav = <<<NAV
+<div class="header d-flex justify-content-between">
+    <p class="invisible"></p>
+    <h1 class="text-white fw-normal py-3 fs-3 mb-0">Küchenseite</h1>
+    <form method="post">
+        <button type="submit" name="logout" id="logoutBt" style="background-color: #6A6A6A" class="shadow-none mx-1 px-1 my-3">
+            <span class="material-icons-outlined" style="color: white">logout</span>
+        </button>
+    </form>
+</div>
+NAV;
+
+$body = <<<BODY
+
+BODY;
+
+print(SiteTemplate::render('Küche - EGS', $nav, $body));
