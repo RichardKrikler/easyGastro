14551b245d8062c1d506a22f505d6c9fd18b076b
Richard Krikler <57064056+RichardKrikler@users.noreply.github.com>
2021-12-21 11:29:51 +0100

    removed unused require

--- src/admin/tables/deleteTable.php
+++ src/admin/tables/deleteTable.php
@@ -6,7 +6,6 @@ use easyGastro\DB_User;
 
 require_once 'DB_Admin_Tables.php';
 require_once __DIR__ . '/../../DB_User.php';
-require_once __DIR__ . '/../../Pages.php';
 
 
 session_start();
--- src/admin/users/deleteUser.php
+++ src/admin/users/deleteUser.php
@@ -4,11 +4,9 @@ header('Location: /admin/users.php');
 
 use easyGastro\admin\users\DB_Admin_Users;
 use easyGastro\DB_User;
-use easyGastro\Pages;
 
 require_once 'DB_Admin_Users.php';
 require_once __DIR__ . '/../../DB_User.php';
-require_once __DIR__ . '/../../Pages.php';
 
 
 session_start();
